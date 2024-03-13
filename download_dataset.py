7ed0594eaa62700e64d8d62e263b5b843c0af5f2
Michael Luciuk <michael.luciuk@gmail.com>
2024-03-13 12:23:21 -0400

    Removing download_file() function

--- download_dataset.py
+++ download_dataset.py
@@ -9,26 +9,6 @@ import requests
 from torch.utils.model_zoo import tqdm
 
 
-def download_file(url: str, local_path: str, chunk_size: int = 1024) -> None:
-    """Download file from the specified URL.
-
-    :param url: The URL of the file to download.
-    :param local_path: The local path where the downloaded file will be saved.
-    :param chunk_size: The size of each data chunk to be downloaded, in bytes. Default is 1024.
-
-    :return: None
-    """
-    response = requests.head(url)
-    n_bytes = int(response.headers.get('Content-Length', 0))
-
-    with requests.get(url, stream=True, timeout=3) as r, open(local_path, 'wb') as out_file, \
-            tqdm(desc="Downloading", total=n_bytes, unit='B', unit_scale=True) as pbar:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            if chunk:
-                out_file.write(chunk)
-                pbar.update(len(chunk))
-
-
 mirror = "https://www.mathworks.com/supportfiles/spc/SpectrumSensing/"
 resource = "SpectrumSenseTrainingDataNetwork.tar.gz"
 file_url = "{}{}".format(mirror, resource)
@@ -46,7 +26,14 @@ if len(os.listdir(target_dir)) != 0:
 
 # Download and extract the dataset into the target directory.
 print(f"Downloading {format(file_url)}")
-download_file(url=file_url, local_path=archive_file)
+n_bytes = int(requests.head(file_url).headers.get('Content-Length', 0))
+
+with (requests.get(file_url, stream=True, timeout=3) as r, open(archive_file, 'wb') as out_file,
+      tqdm(desc="Downloading", total=n_bytes, unit='B', unit_scale=True) as pbar):
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            out_file.write(chunk)
+            pbar.update(len(chunk))
 
 print(f"Extracting to {target_dir}")
 with tarfile.open(archive_file) as tar:
